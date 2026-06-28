19ac484a0b375ab0fed60eb9bf357e1a02d5e4da
Vadim Erenkov <vadimerenkov+github@gmail.com>
2026-06-29 00:15:42 +0400

    Scrollable habit detail screen

--- habits/presentation/src/commonMain/kotlin/vadimerenkov/autasker/habits/presentation/details/HabitDetailsScreen.kt
+++ habits/presentation/src/commonMain/kotlin/vadimerenkov/autasker/habits/presentation/details/HabitDetailsScreen.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
@@ -79,6 +81,7 @@ fun HabitDetailsScreen(
 	Column(
 		verticalArrangement = Arrangement.spacedBy(16.dp),
 		modifier = modifier
+			.verticalScroll(rememberScrollState())
 	) {
 		Text(
 			text = state.habit.title,
